36bdeb9b096ccb7c4977063b2f334811686ed32f
div-styl <gouhmada@gmail.com>
2023-08-25 11:44:37 +0100

    about not linked

---
+++ src/components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About (){
+    return(
+        <h1 className="text-5xl flex justify-center text-lime-500 font-gigachad font-bold">Hello world</h1>
+    );
+}
+
+export default About;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,63 @@
-import React from 'react';
+import React from "react";
+import {Link} from 'react-router-dom'
 
-function Navabr()
-{
-    return (<nav class="bg-gray-300">
+function Navbar() {
+  return (
+    <nav class="bg-gray-300">
+      {/* container */}
+      <div class="flex  items-center justify-between mx-auto p-4">
+        {/* logo */}
+        <a href="/">
+          <ul class="flex items-center  space-x-1">
+            <li>
+              <div class="mr-5px text-4xl font-bold ">LOGO</div>
+            </li>
 
-        {/* container */}
-        <div class="flex  items-center justify-between mx-auto p-4">
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Location_dot_black.svg/1024px-Location_dot_black.svg.png"
+                class="h-8 mr-3"
+                alt="Flowbite Logo"
+              />
+            </li>
+          </ul>
+        </a>
 
-            {/* logo */}
-            <a href="#">
-                <ul class="flex items-center  space-x-1" >
-                    <li>
-                        <div class="mr-5px text-4xl font-bold ">LOGO</div>
-                    </li>
+        <div class="flex items-center mx-10 border-solid	">
+          <ul class="flex items-center font-mono font-medium text-2xl space-x-5">
+            <li>
+              <a href="/" class="hover:text-gray-500">
+                Home
+              </a>
+            </li>
+            {/* <a href="./About" class="hover:text-gray-500">
+              About
+            </a> */}
+            <Link to='/about'>
+                About
+            </Link>
+            <li></li>
 
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Location_dot_black.svg/1024px-Location_dot_black.svg.png" class="h-8 mr-3" alt="Flowbite Logo" />
-                    </li>
-                </ul>
-            </a >
-
-
-
-            <div class="flex items-center mx-10 border-solid	" >
-                <ul class="flex items-center font-mono font-medium text-2xl space-x-5">
-                    <li>
-                        <a href="#" class="hover:text-gray-500">Home</a>
-                    </li>
-
-                    <li>
-                        <a href="#" class="hover:text-gray-500">About</a>
-                    </li>
-
-                    <li>
-                        <a href="#" class="hover:text-gray-500">Contact</a>
-                    </li>
-                    <li>
-
-
-                        <button class="bg-gray-950 text-white hover:bg-gray-600 rounded font-bold py-1.5 px-3 ml-10" >Login</button>
-                    </li>
-                    <li>
-                        <button class="bg-gray-950 text-white hover:bg-gray-600 rounded font-bold py-1.5 px-3">Signup</button>
-                    </li>
-                </ul>
-            </div>
-
-        </div >
-    </nav >
-    );
+            <li>
+              <a href="/" class="hover:text-gray-500">
+                Contact
+              </a>
+            </li>
+            <li>
+              <button class="bg-gray-950 text-white hover:bg-gray-600 rounded font-bold py-1.5 px-3 ml-10">
+                Login
+              </button>
+            </li>
+            <li>
+              <button class="bg-gray-950 text-white hover:bg-gray-600 rounded font-bold py-1.5 px-3">
+                Signup
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navabr;
+export default Navbar;
